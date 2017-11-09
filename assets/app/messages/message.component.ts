2011847d66ca6d234154fd3531b8085fb8130f73
Bohdan <bodya.flesh@gmail.com>
2017-11-09 20:54:46 +0200

    up

--- assets/app/messages/message.component.ts
+++ assets/app/messages/message.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+
 import { Message } from './message.model';
+import { MessageService } from './message.service';
 
 @Component({
     selector: 'app-message',
@@ -10,8 +12,16 @@ export class MessageComponent{
     @Input() message: Message;
     @Output() editClicked = new EventEmitter<String>();
 
+    constructor(private messageService : MessageService){
+
+    }
+
     onEdit(){
         console.log('click!');
         this.editClicked.emit('A new value');
+    }
+
+    onDelete(){
+        this.messageService.deleteMessage(this.message);
     }
 }
