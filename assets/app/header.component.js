62487ff5cda6a060d36c7ced7f267bf7e9dd5bd9
Bohdan <bodya.flesh@gmail.com>
2017-11-10 23:54:37 +0200

    up

--- assets/app/header.component.js
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-var HeaderComponent = /** @class */ (function () {
-    function HeaderComponent() {
-    }
-    return HeaderComponent;
-}());
-export { HeaderComponent };
--- assets/app/messages/messages.component.js
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-var MessagesComponent = /** @class */ (function () {
-    function MessagesComponent() {
-    }
-    return MessagesComponent;
-}());
-export { MessagesComponent };
